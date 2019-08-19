5b1c09db3a0406195afa6bf8a8b347f0b8419dc9
xyyojl <m13631897256@163.com>
2019-08-19 11:10:56 +0800

    todo 增删改查模拟，模拟数据，渲染到页面，点击创建任务，新增 todo，点击todo，完成任务

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,15 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    lists:[],
-    visible:true
+    lists:[
+      { id: 1, text: '快乐不是因为得到的多而是因为计较的少！', finished: true },
+      { id: 2, text: '我不去想是否能够成功，既然选择了远方，便只顾风雨兼程！', finished: false },
+      { id: 3, text: '如果心胸不似海，又怎能有海一样的事业。', finished: true },
+      { id: 4, text: '学习要加，骄傲要减，机会要乘，懒惰要除', finished: false },
+      { id: 5, text: '你可以很有个性，但某些时候请收敛。', finished: false }
+    ],
+    visibleConfirm:false
   },
-  confirm(event){
-    // 拿到内容生成todo
-    console.log(event.detail)
+  confirmCreateTodo(event){
+    let content = event.detail;
+    if(content){
+      let todo = [{ id: this.data.lists.length + 1, text: content, finished: false }]
+      this.data.lists = todo.concat(this.data.lists)
+      this.setData({ lists: this.data.lists})
+      this.hideConfirm()
+    }
   },
-  cancel(){
-    this.setData({'visible':false})
+  deleteTodo(event){
+    let index = event.currentTarget.dataset.index
+    this.data.lists[index].finished = true
+    this.setData({ lists: this.data.lists}) 
+  },
+  hideConfirm(){
+    this.setData({ 'visibleConfirm':false })
+  },
+  showConfirm(){
+    this.setData({ 'visibleConfirm': true })
   },
   /**
    * 生命周期函数--监听页面加载
