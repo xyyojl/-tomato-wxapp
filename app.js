b9ff440b61068114b4b06b069848107b20eb216d
xyyojl <m13631897256@163.com>
2019-08-20 18:31:19 +0800

    rm app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com', 
-    app_id: "wxd6d0e7012751ca71",
-    app_secret: "0089e5e8189b03e1137a9fd54e1f2c6a",
-    t_app_id: "XjAFbhSsV4GAH5wamtdNNmtV",
-    t_app_secret: "RK3pFKPJrWmExioCXskhXEAN"
-  }
-})
