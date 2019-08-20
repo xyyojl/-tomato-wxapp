0236d0fbdc2c336a3c890ad4d6017e8e614b3e1b
xyyojl <m13631897256@163.com>
2019-08-20 17:12:03 +0800

    封装 http，待测试

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
   globalData: {
-    userInfo: null
+    host: 'https://gp-server.hunger-valley.com', 
+    app_id: "wxd6d0e7012751ca71",
+    app_secret: "0089e5e8189b03e1137a9fd54e1f2c6a",
+    t_app_id: "XjAFbhSsV4GAH5wamtdNNmtV",
+    t_app_secret: "RK3pFKPJrWmExioCXskhXEAN"
   }
 })
